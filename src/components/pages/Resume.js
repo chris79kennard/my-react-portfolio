21b60be5f6671409b622cd3004f5b48992e4fd3e
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-12-01 23:44:42 -0600

    added resume file.

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,7 +2,18 @@ import React from "react"
 
 
 export default function Resume (){
-  return (null)
+  return (
+    <div>
+      <a href="/images/Christopher-Kennard.pdf" target="_blank">Click Here to view Christopher's Resume!</a>
+      <ul>
+        <li>
+      FrontEnd: CSS HTML JAVASCRIPT
+      </li>
+      <li>BackEnd: idk yet. </li>
+      </ul>
+    </div>
+
+  )
 
   
 }
