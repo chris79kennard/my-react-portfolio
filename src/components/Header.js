8a8b1ecaa23fcabd613e54f8d8d1c41ca50e30c8
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-12-01 22:00:04 -0600

    inital commit, set up file stucter and imports

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Aboutme from "./pages/Aboutme"
+import  Contact from "./pages/Contact"
+import Projects from "./pages/Projects"
+import Resume from "./pages/Resume"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+
+export default function Header (){
+  return (null)
+
+  
+}
