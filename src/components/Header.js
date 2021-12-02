5476d135d501ff68e0a4e03da805f302d1e42a88
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-12-01 22:31:49 -0600

    created nav bar text is displaying for About me tab

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{useState} from "react"
 import Aboutme from "./pages/Aboutme"
 import  Contact from "./pages/Contact"
 import Projects from "./pages/Projects"
@@ -6,8 +6,34 @@ import Resume from "./pages/Resume"
 import Footer from "./Footer"
 import Navbar from "./Navbar"
 
+
+
 export default function Header (){
-  return (null)
+    const [currentPage, setCurrentPage] = useState('Home');
+  
+    // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+    const renderPage = () => {
+      if (currentPage === 'Aboutme') {
+        return <Aboutme />;
+      }
+      if (currentPage === 'Projects') {
+        return <Projects />;
+      }
+      if (currentPage === 'Resume') {
+        return <Resume />;
+      }
+      return <Contact />;
+    };
+  
+    const handlePageChange = (page) => setCurrentPage(page);
+  return (
+    <div>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange}/>
+      {renderPage()}
+      <Footer/>
+    </div>
+
+  )
 
   
 }
